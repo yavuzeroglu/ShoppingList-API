0274071d282ef62c4afe2f8317e04446c22b01ec
yavuz-eroglu <yvzeroglu16@gmail.com>
2025-05-02 15:51:32 +0300

    feat: Add BasketUser role enum and entity

--- src/Core/ShoppingList.Domain/Entities/BasketUser.cs
+++ src/Core/ShoppingList.Domain/Entities/BasketUser.cs
@@ -1,18 +1,15 @@
 using ShoppingList.Domain.Entities.Common;
 using ShoppingList.Domain.Entities.Identity;
+using ShoppingList.Domain.Enums;
+
 
 namespace ShoppingList.Domain.Entities;
 
 public class BasketUser : BaseEntity
 {
-  public BasketUser()
-  {
-    Basket = new();
-    AppUser = new();
-  }
-
   public int BasketId { get; set; }
   public string AppUserId { get; set; } = string.Empty;
+  public BasketUserRole Role { get; set; } = BasketUserRole.Viewer;
   
   // Navigation Properties
   public Basket Basket { get; set; }
